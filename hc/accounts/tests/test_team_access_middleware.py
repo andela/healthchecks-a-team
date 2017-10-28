f33a3e349cebca5fd0861d5381eaceedd57808a0
kimotho-njoki <graycenjoki@gmail.com>
2017-10-28 12:03:53 +0300

    [Chore] Fix pylint errors to follow PEP8 rules

--- hc/accounts/tests/test_team_access_middleware.py
+++ hc/accounts/tests/test_team_access_middleware.py
@@ -4,16 +4,20 @@ from hc.accounts.models import Profile
 
 
 class TeamAccessMiddlewareTestCase(TestCase):
-
+    """
+    This class represents team access middleware test case
+    """
     def test_it_handles_missing_profile(self):
+        """
+        Test that a missing profile is taken care of
+        """
         user = User(username="ned", email="ned@example.org")
         user.set_password("password")
         user.save()
 
         self.client.login(username="ned@example.org", password="password")
-        r = self.client.get("/about/")
-        self.assertEqual(r.status_code, 200)
+        res = self.client.get("/about/")
+        self.assertEqual(res.status_code, 200)
 
         ### Assert the new Profile objects count
-        self.assertEqual(Profile.objects.count(), 1) 
-        
+        self.assertEqual(Profile.objects.count(), 1)
