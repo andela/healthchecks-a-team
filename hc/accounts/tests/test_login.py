0462442b6155573c7c733d0d513e20ce1dcb57df
kimotho-njoki <graycenjoki@gmail.com>
2017-10-28 11:59:33 +0300

    [Chore] Add assert statements

--- hc/accounts/tests/test_login.py
+++ hc/accounts/tests/test_login.py
@@ -5,8 +5,14 @@ from hc.api.models import Check
 
 
 class LoginTestCase(TestCase):
+    """
+    This class represents the login test case
+    """
 
     def test_it_sends_link(self):
+        """
+        Test a link is sent via email
+        """
         check = Check()
         check.save()
 
@@ -15,26 +21,33 @@ class LoginTestCase(TestCase):
         session.save()
 
         form = {"email": "alice@example.org"}
+        count0 = User.objects.count()
 
-        r = self.client.post("/accounts/login/", form)
-        assert r.status_code == 302
+        res = self.client.post("/accounts/login/", form)
+        assert res.status_code == 302
+        count1 = User.objects.count()
 
         ### Assert that a user was created
-        user = User.objects.get(email="alice@example.org")  
+        self.assertEqual(count1, count0+1)
+        user = User.objects.get(email="alice@example.org")
         self.assertEqual(user.email, "alice@example.org")
 
         # And email sent
         self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(mail.outbox[0].subject, 'Log in to healthchecks.io')
+        self.assertEqual(mail.outbox[0].subject,
+                         'Log in to healthchecks.io')
         ### Assert contents of the email body
-        self.assertIn('To log into healthchecks.io, please open the link below:', mail.outbox[0].body) 
+        self.assertIn('To log into healthchecks.io',
+                      mail.outbox[0].body)
 
         ### Assert that check is associated with the new user
         check = Check.objects.get(user=user)
         self.assertEqual(check.user, user)
 
     def test_it_pops_bad_link_from_session(self):
+        """
+        Test a bad link is popped from session
+        """
         self.client.session["bad_link"] = True
         self.client.get("/accounts/login/")
         assert "bad_link" not in self.client.session
-
