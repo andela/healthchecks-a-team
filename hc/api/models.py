4be55dc7b4381939cbd5642bd53b37244e322dde
Vincent Asante <vincenthokie@gmail.com>
2017-11-03 09:50:02 +0300

    [Feature #151946830] refactor previous nag mode switch to a nag mode notification to show if a nag is happening

--- hc/api/models.py
+++ hc/api/models.py
@@ -52,7 +52,6 @@ class Check(models.Model):
     last_ping = models.DateTimeField(null=True, blank=True)
     alert_after = models.DateTimeField(null=True, blank=True, editable=False)
     status = models.CharField(max_length=6, choices=STATUSES, default="new")
-    nag_mode = models.BooleanField(default=True)
 
     def name_then_code(self):
         if self.name:
